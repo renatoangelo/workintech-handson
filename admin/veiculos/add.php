0fa0068310f8c0b547914faac7f517b6b17767db
renatoangelo <renatoangelo@gmail.com>
2023-09-13 15:20:48 -0300

    Update

--- admin/veiculos/add.php
+++ admin/veiculos/add.php
@@ -1,186 +1,116 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include("../include/default.php");
 ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
+  <div class="wrapper">
 
-<?php include("../common/navbar.php"); ?>
+    <?php include("../common/navbar.php"); ?>
 
-<?php include("../common/sidebar.php"); ?>
+    <?php include("../common/sidebar.php"); ?>
 
- <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">WorkinTech - HandsOn</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="../dashboard/index.php">Home</a></li>
-              <li class="breadcrumb-item active">Veiculos</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">WorkinTech - HandsOn</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="../dashboard/index.php">Home</a></li>
+                <li class="breadcrumb-item active">Cadastro de Alunos</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
 
-        <div class="card card-primary">
-          <!-- .card-header -->
-          <div class="card-header">
-            <h3 class="card-title"><i class="fa-solid fa-house"></i> Cadastre um Veiculo</h3>
-          </div>
-
-
-          <?php
-          $timezone = new DateTimeZone('America/Sao_Paulo');
-          $data_agora = new DateTime('now', $timezone);
-          $data_agora = $data_agora->format('Y-m-d H:i:s');
-
-          $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-          //var_dump($dados);
-
-          if(!empty($dados['cadastrar'])) {
-            $query_imovel = "INSERT INTO real_estates (id, user_id, title, slug, type, transaction_type, fase, created_at, updated_at)
-                              VALUES ('', :user_id, :titulo, :slug, :tipoImovel, :finalidade, :fase, :data_agora, :data_agora2)";
-            $result_imovel = $conn->prepare($query_imovel);
-            $result_imovel->bindParam(':user_id', $_SESSION['id'], PDO::PARAM_STR);
-            $result_imovel->bindParam(':titulo', $dados['titulo'], PDO::PARAM_STR);
-            $result_imovel->bindParam(':slug', $dados['slug'], PDO::PARAM_STR);
-            $result_imovel->bindParam(':tipoImovel', $dados['tipoImovel'], PDO::PARAM_STR);
-            $result_imovel->bindParam(':finalidade', $dados['finalidade'], PDO::PARAM_INT);
-            $result_imovel->bindParam(':fase', $dados['fase'], PDO::PARAM_INT);
-            $result_imovel->bindParam(':data_agora', $data_agora, PDO::PARAM_STR);
-            $result_imovel->bindParam(':data_agora2', $data_agora, PDO::PARAM_STR);
-            
-            if ($result_imovel->execute()) {
-              $_SESSION['msg'] = "<p style='color: #32c330;text-align: center;font-weight: bold;'>Imóvel cadastrado com Sucesso!</p>";
-              header("Location: ../veiuclos/list.php");
-        
-            } else {
-              $_SESSION['msg'] = "<p style='color: #ff0000;text-align: center;font-weight: bold;'>ERRO: Tente Novamente!</p>"; 
-            }
-
-          } else {
-
-          }
-          ?>
-
-          <!-- form start -->
-
-          <form action="" method="post">
-            <div class="card-body">
-              <!-- row -->
-              <?php
-            if (isset($_SESSION['msg'])) {
-              echo $_SESSION['msg'];
-              unset($_SESSION['msg']);
-            }
-          ?>
-              <div class="row">
-                <section class="col-lg-6 connectedSortable">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Título</label>
-                    <input type="text" class="form-control" id="titulo" placeholder="Insira o Título do Imóvel aqui" name="titulo">
-                  </div>
-                </section>
-
-                <section class="col-lg-6 connectedSortable">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Slug <i class="fa-solid fa-circle-info"></i></label>
-                    <input
-                    type="text" 
-                    name="slug"
-                    class="form-control " 
-                    id="slug"
-                    placeholder="Exemplo: imóvel-maringa-8-quartos"
-                    required
-                    minlength="5"
-                    maxlength="100"
-                    value="" >
-                    <span class="text-sm text-primary">Não utilize acento(s) e espaços</span>
-                  </div>
-                </section>
-
-
-                <div class="form-group mt-4 col-md-4">
-                  <label for="reason">Finalidade:</label>
-                  <select name="finalidade" id="reason" class="form-control ">
-                      <option value="0"  selected >Residencial</option>
-                      <option value="1" >Comercial</option>
-                      <option value="2" >industrial</option>
-                  </select>
-                </div>
-
-                <div class="form-group mt-4 col-md-4">
-                    <label for="estateType">Tipo de imóvel:</label>
-                    <select name="tipoImovel" id="estateType" class="form-control ">
-                        <option value="apartamento" >Apartamento</option>
-                        <option value="casa" >Casa</option>
-                        <option value="chacara" >Chácara</option>
-                        <option value="terreno" >Terreno</option>
-                        <option value="kitnet" >Kitnet</option>
-                        <option value="flat" >Flat</option>
-                        <option value="lancamento" >Lançamento</option>
-                        <option value="empreendimento" >Empreendimento</option>
-                        <option value="comercial" >Comercial</option>
-                        <option value="industrial" >Industrial</option>
-                    </select>
-                </div>
-
-                <div class="for-group mt-4 col-md-2">
-                    <label for="">Fase</label>
-                    <select name="fase" id="faseRealEstate" class="form-control ">
-                        <option value="0" >Pronto</option>
-                        <option value="1" >Em construção</option>
-                    </select>
-                </div>
-
-                <section class="col-lg-12 connectedSortable">
-                  <div class="row">
-                    <button type="submit" name="cadastrar" value="cadastrar" class="btn btn-primary btn-block">Cadastrar <i class="fa-solid fa-right-to-bracket"></i></button>
-                  </div>
-                  
-                </section>
-              </div>
-              <!-- /row -->
+          <div class="card card-primary">
+            <!-- .card-header -->
+            <div class="card-header">
+              <h3 class="card-title"> <i class="fa-solid fa-user"></i> Cadastre um Aluno</h3>
             </div>
-            <!-- /.card-body -->
-          </form>
-    </div>
-        <!-- Main row -->
 
-        <div class="row">
-          <!-- Left col -->
-          <section class="col-lg-7 connectedSortable">
-            <!-- 
+
+            <?php
+            $timezone = new DateTimeZone('America/Sao_Paulo');
+            $data_agora = new DateTime('now', $timezone);
+            $data_agora = $data_agora->format('Y-m-d H:i:s');
+
+            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+            var_dump($dados);
+            echo $data_agora;
+
+
+
+            if (!empty($dados['cadastrar'])) {
+              $query_cadastro = "INSERT INTO cadastro (nome, ra, turma, oficina, notebook, termos, data_agora) VALUES (:nome, :ra, :turma, :oficina, :notebook, :termos, :data_agora)";
+              
+              $result_cadastro = $conn->prepare($query_cadastro);
+              $result_cadastro->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+              $result_cadastro->bindParam(':ra', $dados['ra'], PDO::PARAM_STR);
+              $result_cadastro->bindParam(':turma', $dados['turma'], PDO::PARAM_STR);
+              $result_cadastro->bindParam(':oficina', $dados['oficina'], PDO::PARAM_STR);
+              $result_cadastro->bindParam(':notebook', $dados['notebook'], PDO::PARAM_INT);
+              $result_cadastro->bindParam(':termos', $dados['termos'], PDO::PARAM_INT);
+              $result_cadastro->bindParam(':data_agora', $data_agora, PDO::PARAM_STR);
+              
+              if ($result_cadastro->execute()) {
+                //$_SESSION['msg'] = "<p style='color: #32c330;text-align: center;font-weight: bold;'>Imóvel cadastrado com Sucesso!</p>";
+                //header("Location: ../veiuclos/list.php");
+                echo "DEU CERTO!";
+              } else {
+                //$_SESSION['msg'] = "<p style='color: #ff0000;text-align: center;font-weight: bold;'>ERRO: Tente Novamente!</p>";
+                echo "DEU ERRO!!!!";
+              }
+            } else {
+              echo("Caiu no else");
+            }
+
+            ?>
+
+            <!-- forms start -->
+            <?php
+            include("../../forms/form.php");
+            ?>
+            <!-- Forms End -->
+          </div>
+          <!-- Main row -->
+
+          <div class="row">
+            <!-- Left col -->
+            <section class="col-lg-7 connectedSortable">
+              <!-- 
               LADO ESQUERDO
               CONTEÚDO VAI AQUI
             -->
-          </section>
-          <!-- /.Left col -->
-          <!-- right col (We are only adding the ID to make the widgets sortable)-->
-          <section class="col-lg-5 connectedSortable">
-            <!-- 
+            </section>
+            <!-- /.Left col -->
+            <!-- right col (We are only adding the ID to make the widgets sortable)-->
+            <section class="col-lg-5 connectedSortable">
+              <!-- 
               LADO DIREITO
               CONTEÚDO VAI AQUI
             -->
-          </section>
-          <!-- right col -->
-        </div>
-        <!-- /.row (main row) -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
+            </section>
+            <!-- right col -->
+          </div>
+          <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+    </div>
 
-  <?php  include("../common/footer.php"); ?>
+    <?php include("../common/footer.php"); ?>
---
+++ forms/form.php
@@ -0,0 +1,108 @@
+<!-- form start -->
+<form action="" method="post">
+  <div class="card-body">
+    <!-- row -->
+    <?php
+    if (isset($_SESSION['msg'])) {
+      echo $_SESSION['msg'];
+      unset($_SESSION['msg']);
+    }
+    $query_oficina = 'SELECT o.id_oficina, o.nome 
+    FROM oficinas o 
+    WHERE o.limite > (
+                        SELECT count(*) 
+                        FROM cadastro c 
+                        WHERE c.oficina = o.id_oficina
+                      )';
+    $oficinas_disponiveis = $conn->prepare($query_oficina);
+    $oficinas_disponiveis->execute();
+    $oficinas_disponiveis = $oficinas_disponiveis->fetchAll();
+    ?>
+    <div class="row">
+      <section class="col-lg-6 connectedSortable">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Nome Completo: </label>
+          <input type="text" class="form-control" id="nome" placeholder="Digite o seu nome" name="nome">
+        </div>
+      </section>
+
+      <section class="col-lg-6 connectedSortable">
+        <div class="form-group">
+          <label for="exampleInputEmail1">RA <i class="fa-solid fa-circle-info"></i></label>
+          <input type="text" name="ra" class="form-control " id="ra" placeholder="Apenas Números" required minlength="9" maxlength="9" value="">
+          <span class="text-sm text-primary">Utilize apenas números</span>
+        </div>
+      </section>
+
+      <div class="form-group mt-12 col-md-12">
+        <label for="estateType">Turma</label>
+        <select name="turma" id="turma" class="form-control ">
+          <option value="adsis2a">ADSIS2-N-A</option>
+          <option value="adsis2b">ADSIS2-N-B</option>
+          <option value="adsis2c">ADSIS2-N-C</option>
+
+          <option value="adsis4a">ADSIS4S-N-A</option>
+          <option value="adsis6a">ADSIS6S-N-A</option>
+
+          <option value="esoft2a">ESOFT2S-N-A</option>
+          <option value="esoft2b">ESOFT2S-N-B</option>
+          <option value="esoft2c">ESOFT2S-N-C</option>
+          <option value="esoft2d">ESOFT2S-N-D</option>
+          <option value="esoft4a">ESOFT4S-N-A</option>
+          <option value="esoft4b">ESOFT4S-N-B</option>
+          <option value="esoft4c">ESOFT4S-N-C</option>
+          <option value="esoft6a">ESOFT6S-N-A</option>
+          <option value="esoft8a">ESOFT8S-N-A</option>
+        </select>
+      </div>
+
+      <div class="form-group mt-12 col-md-12">
+        <label for="estateType">Oficina</label>
+        <select name="oficina" id="oficina" class="form-control ">
+          <?php
+            foreach ($oficinas_disponiveis as $oficina) { 
+          ?>
+            <option value="<?=$oficina['id_oficina'];?>"><?=$oficina['nome'];?></option>
+          <?php 
+          }
+          ?>
+        </select>
+      </div>
+
+      <div class="form-group mt-12 col-md-12">
+        
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="notebook" id="notebook" value='1'>
+          <label class="form-check-label" for="flexRadioDefault1">
+            Possuo <b>SIM</b> um Notebook
+          </label>
+        </div>
+
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="notebook" id="notebook2" value='2' checked>
+          <label class="form-check-label" for="flexRadioDefault2">
+            <b>Não </b>possuo um Notebook
+          </label>
+        </div>
+      </div>
+
+      <div class="form-group mt-12 col-md-12">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="1" id="termos" name="termos" checked>
+          <label class="form-check-label" for="flexCheckChecked">
+            Aceito os termos do Evento
+          </label>
+        </div>
+      </div>
+
+      <section class="col-lg-12 connectedSortable">
+        <div class="row">
+          <button type="submit" name="cadastrar" value="cadastrar" class="btn btn-primary btn-block">Realizar Inscrição <i class="fa-solid fa-right-to-bracket"></i></button>
+        </div>
+
+      </section>
+    </div>
+    <!-- /row -->
+  </div>
+  <!-- /.card-body -->
+</form>
