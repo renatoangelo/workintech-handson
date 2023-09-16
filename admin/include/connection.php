266c90244a298e6f21d8fdd2c676a64ca1eb5157
renatoangelo <renatoangelo@gmail.com>
2023-09-16 01:41:00 -0300

    UTF na conn

--- admin/include/connection.php
+++ admin/include/connection.php
@@ -9,7 +9,7 @@ $port = PORT;
 
 
 try {
-  $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
