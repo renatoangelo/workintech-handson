5707dae99e75a2023ec1548c38f630d3abb7fdec
renatoangelo <renatoangelo@gmail.com>
2023-09-16 00:50:38 -0300

    Final cadastro com mensgaens final

--- landingpage/head.php
+++ landingpage/head.php
@@ -7,11 +7,11 @@
 
     <meta name="description" content="Workintech é uma feira de empregabilidae que acontece para os alunos de Tecnologia dos cursos noturnos na faculdade">
     <meta name="author" content="Alunos de Tecnologia Noturno da Faculdae">
-    <meta name="msapplication-TileImage" content="https://devinsider.com.br/workintech/images/icon2.png" />
-    <meta property="og:image" content="https://devinsider.com.br/workintech/images/icon2.png">
+    <meta name="msapplication-TileImage" content="https://devinsider.com.br/workintechhandson/landingpage/images/icon2.png" />
+    <meta property="og:image" content="https://devinsider.com.br/workintechhandson/landingpage/images/icon2.png">
     <meta property="og:image:type" content="image/jpg">
     <meta property="og:description" content="Feira de Empregabilidade que acontece para os alunos de Tecnologia dos Cursos Noturnos">
-    <meta property="og:url" content="https://devinsider.com.br/workintech/" >
+    <meta property="og:url" content="https://devinsider.com.br/workintechhandson/landingpage" >
     <meta property="og:site_name" content="WorkInTech - Feira da Empregabilidade" >
     <meta property="og:title" content="Aproveite a Feira de Empregabilidade WorkInTech" >
 
